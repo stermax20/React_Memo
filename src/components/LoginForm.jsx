9da3a46d9fd4da82928341d6ecf68c74ea20ff4b
stermax20 <kimminseok0911@gmail.com>
2024-06-11 01:29:21 +0900

    FEAT: Memo Front-End 업로드.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -10,11 +10,16 @@ const LoginForm = () => {
     e.preventDefault();
     try {
       const response = await authService.login(email, password);
-      console.log(response.data);
-      alert("로그인에 성공하였습니다.");
+      if (response.status === 200) {
+        alert("로그인에 성공하였습니다.");
+      }
     } catch (error) {
-      console.error(error);
-      alert("로그인에 실패하였습니다.");
+      console.error('Login error:', error);
+      if (error.response && error.response.status === 401) {
+        alert("잘못된 이메일 또는 비밀번호입니다.");
+      } else {
+        alert("로그인에 실패하였습니다.");
+      }
     }
   };
 
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import FormInput from "./FormInput";
 import authService from "../api/axios";
+import { useNavigate } from 'react-router-dom';
 
 const SignupForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
     try {
       const response = await authService.signup(email, password, name);
-      console.log(response.data);
-      alert("회원가입에 성공하였습니다.");
+      console.log(response)
+      if(response.status === 200) {
+        alert("회원가입에 성공하였습니다.");
+        navigate("/login")
+      }
     } catch (error) {
       console.error(error);
       alert("회원가입에 실패하였습니다.");
@@ -39,6 +45,7 @@ const SignupForm = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
+      
       <button type="submit">회원가입</button>
     </form>
   );
